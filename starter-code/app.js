b2c1392301708debd86757af61755365aef555ed
mariehardy <mademoiselle.mph@gmail.com>
2020-04-02 22:04:35 +0200

    BER WDPT marie - beerAPI not working...

--- starter-code/app.js
+++ starter-code/app.js
@@ -6,6 +6,7 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+// set up handlebars template language
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -15,5 +16,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
+app.get('/beers', (req, res) => {
+
+    punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      console.log('Beers from the database: ', beersFromApi)
+      res.render('beers', { beersArray: beersFromApi })
+    })
+
+})
+
+app.get('/random-beers', (req, res) => res.render('randombeers'));
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
